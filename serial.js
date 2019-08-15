b539f1200854e7aea1b3b1fe7bfa9c8f8cd28e78
devGW <doscmDev@gmail.com>
2019-08-16 04:07:08 +0900

    modify: function style

--- serial.js
+++ serial.js
@@ -1,16 +1,18 @@
 const SerialPort = require("serialport");
 
 portName = "/dev/ttys002";
-sp = new SerialPort(portName);
+port = new SerialPort(portName, {
+  baudRate: 9600
+});
 
-sp.on("open", () => {
+port.on("open", () => {
   console.log("Serial Port open");
 
-  sp.on("data", function(data) {
+  port.on("data", data => {
     console.log("value: ", String.fromCharCode(...data));
   });
 
-  sp.write("Hello world", error => {
+  port.write("Hello world", error => {
     error
       ? console.log("Error on write : ", error.message)
       : console.log("입력 완료");
